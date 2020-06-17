6f27c823a67b7b1cc7872d7782316eb87c29bda3
liuzhichao <liuzc6788@163.com>
2020-06-17 10:17:11 +0800

    upgrade package

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueCompositionApi from '@vue/composition-api';
+import VueCompositionApi from "@vue/composition-api";
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
