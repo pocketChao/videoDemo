90cff0aefb80dcb6ebc98f6b80082c285efb889f
liuzhichao <liuzc6788@163.com>
2020-08-31 16:34:33 +0800

    多组件过渡

---
+++ src/components/custom-image.vue
@@ -0,0 +1,20 @@
+<template>
+  <l-c-image
+    style="margin-top: 20px"
+    width="80"
+    height="80"
+    src="https://img.yzcdn.cn/vant/cat.jpeg"
+  ></l-c-image>
+</template>
+
+<script>
+import { Image } from "vant";
+export default {
+  name: "custom-image",
+  components: {
+    LCImage: Image
+  }
+};
+</script>
+
+<style scoped></style>
